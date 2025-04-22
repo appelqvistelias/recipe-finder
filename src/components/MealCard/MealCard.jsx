edb6aaf33f9245f72a3d1480cd4d6413ac5470c9
Skjesper <jesperskeppstedt@gmail.com>
2025-04-22 14:51:56 +0200

    added padding and buttons to mealcard

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -1,18 +1,23 @@
 
 import React from 'react';
 import styles from './MealCard.module.css';
+import Button from '../Button/Button';
 
 export default function MealCard({ meal }) {
     return (
       <section className={styles.mealCard}>
+        <div className={styles.imgContainer}>
         <img src={meal.image} alt={meal.label} className={styles.mealImage} />
-        <div className={styles.mealInfo}>
+
+        </div>
+        <section className={styles.mealInfo}>
           <h2>{meal.label}</h2>
           <p>{meal.dietLabels.join(", ")}</p>
-          <a href={meal.url} target="_blank" rel="noopener noreferrer">
-            Visa recept
-          </a>
-        </div>
+           <Button
+                  title="Show recipe"
+                />
+          
+        </section>
       </section>
     );
   }
