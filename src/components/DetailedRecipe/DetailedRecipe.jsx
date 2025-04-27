e23dfa83fe5f5b33531f183bb9bbe722302a09fc
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-27 15:30:36 +0200

    added more meal info

--- src/components/DetailedRecipe/DetailedRecipe.jsx
+++ src/components/DetailedRecipe/DetailedRecipe.jsx
@@ -6,6 +6,14 @@ export default function DetailedRecipe({ recipe }) {
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1>{recipe.label}</h1>
+        <div className={styles.recipeInfo}>
+          <p>Cuisine Type: {recipe.cuisineType?.join(", ")}</p>
+          <p>Meal Type: {recipe.mealType?.join(", ")}</p>
+          <div className={styles.yieldAndTime}>
+            <p>Yield: {recipe.yield} persons</p>
+            <p>Time: {recipe.totalTime} min</p>
+          </div>
+        </div>
         <img src={recipe.image} alt={recipe.label} />
         <h2>Ingredients</h2>
         <ul>
@@ -13,6 +21,8 @@ export default function DetailedRecipe({ recipe }) {
             <li key={index}>{line}</li>
           ))}
         </ul>
+        <UnitConverter />
+
         {recipe.totalNutrients && (
           <div className={styles.nutrients}>
             <h2>Nutrients</h2>
@@ -27,7 +37,14 @@ export default function DetailedRecipe({ recipe }) {
             })}
           </div>
         )}
-        <UnitConverter />
+        <h2>Tags</h2>
+        <div className={styles.healthLabels}>
+          {recipe.healthLabels?.map((label, index) => (
+            <p key={index} className={styles.healthLabelTag}>
+              {label}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
