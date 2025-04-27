4b8df9b13c64a16849df779a4c7c79972e03b849
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-27 16:15:46 +0200

    changed some basic composition

--- src/components/DetailedRecipe/DetailedRecipe.jsx
+++ src/components/DetailedRecipe/DetailedRecipe.jsx
@@ -10,59 +10,61 @@ export default function DetailedRecipe({ recipe }) {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <h1>{recipe.label}</h1>
-        <div className={styles.recipeInfo}>
-          <p>
-            Cuisine Type:{" "}
-            {recipe.cuisineType
-              ?.map((type) => capitalizeFirstLetter(type))
-              .join(", ")}
-          </p>
-          <p>
-            Meal Type:{" "}
-            {recipe.mealType
-              ?.map((type) => capitalizeFirstLetter(type))
-              .join(", ")}
-          </p>
-          <div className={styles.yieldAndTime}>
-            <p>Yield: {recipe.yield} people</p>
-            {recipe.totalTime && recipe.totalTime > 0 ? (
-              <p>Time: {recipe.totalTime} min</p>
-            ) : null}
-          </div>
-        </div>
-        <img src={recipe.image} alt={recipe.label} />
-        <h2>Ingredients</h2>
-        <ul className={styles.ingredientsList}>
-          {recipe.ingredientLines.map((line, index) => (
-            <li key={index}>{line}</li>
-          ))}
-        </ul>
-        <UnitConverter />
-
-        {recipe.totalNutrients && (
-          <div className={styles.nutrients}>
-            <h2>Nutrients</h2>
-            {["CHOCDF", "PROCNT", "FAT"].map((key) => {
-              const nutrient = recipe.totalNutrients[key];
-              return nutrient ? (
-                <p key={key}>
-                  {nutrient.label}: {Math.round(nutrient.quantity)}{" "}
-                  {nutrient.unit}
-                </p>
-              ) : null;
-            })}
-          </div>
-        )}
-        <h2>Tags</h2>
-        <div className={styles.healthLabels}>
-          {recipe.healthLabels?.map((label, index) => (
-            <p key={index} className={styles.healthLabelTag}>
-              {label}
+    <div className={styles.outerContainer}>
+      <div className={styles.innerContainer}>
+        <div className={styles.wrapper}>
+          <h1>{recipe.label}</h1>
+          <div className={styles.recipeInfo}>
+            <p>
+              Cuisine Type:{" "}
+              {recipe.cuisineType
+                ?.map((type) => capitalizeFirstLetter(type))
+                .join(", ")}
             </p>
-          ))}
+            <p>
+              Meal Type:{" "}
+              {recipe.mealType
+                ?.map((type) => capitalizeFirstLetter(type))
+                .join(", ")}
+            </p>
+            <div className={styles.yieldAndTime}>
+              <p>Yield: {recipe.yield} people</p>
+              {recipe.totalTime && recipe.totalTime > 0 ? (
+                <p>Time: {recipe.totalTime} min</p>
+              ) : null}
+            </div>
+          </div>
+          <img src={recipe.image} alt={recipe.label} />
+          <h2>Ingredients</h2>
+          <ul className={styles.ingredientsList}>
+            {recipe.ingredientLines.map((line, index) => (
+              <li key={index}>{line}</li>
+            ))}
+          </ul>
+          <UnitConverter />
+
+          {recipe.totalNutrients && (
+            <div className={styles.nutrients}>
+              <h2>Nutrients</h2>
+              {["CHOCDF", "PROCNT", "FAT"].map((key) => {
+                const nutrient = recipe.totalNutrients[key];
+                return nutrient ? (
+                  <p key={key}>
+                    {nutrient.label}: {Math.round(nutrient.quantity)}{" "}
+                    {nutrient.unit}
+                  </p>
+                ) : null;
+              })}
+            </div>
+          )}
+          <h2>Tags</h2>
+          <div className={styles.healthLabels}>
+            {recipe.healthLabels?.map((label, index) => (
+              <p key={index} className={styles.healthLabelTag}>
+                {label}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
