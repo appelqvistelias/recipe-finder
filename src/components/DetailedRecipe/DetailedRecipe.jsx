85164b0aa98e56be7dbb7a98c28b540516fe0d3f
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-28 19:23:50 +0200

    minor code formation tweak

--- src/components/DetailedRecipe/DetailedRecipe.jsx
+++ src/components/DetailedRecipe/DetailedRecipe.jsx
@@ -15,89 +15,87 @@ export default function DetailedRecipe({ recipe }) {
     <div className={styles.outerContainer}>
       <div className={styles.innerContainer}>
         <div className={styles.wrapper}>
-        <Button 
-          onClick={() => (window.location.href = "/")}
-          aria-label="Back to home page"
-          title="back"
-          size="large"
-          style={{ marginTop: '1em', }}
-        >
-          Back to Home
-        </Button>
+          <Button
+            onClick={() => (window.location.href = "/")}
+            aria-label="Back to home page"
+            title="↩ Back"
+            size="large"
+            style={{ marginTop: "3rem" }}
+          />
 
-        <div className={styles.recipeHeader}>
-          <h2>{recipe.label}</h2>
+          <div className={styles.recipeHeader}>
+            <h2>{recipe.label}</h2>
           </div>
           <div className={styles.recipeContentWrapper}>
-          
-          
-          <div className={styles.recipeImg}>
-            <img src={recipe.image} alt={`Image of ${recipe.label}`} />
-          </div>
-         
-          <section className={styles.recipeInfoWrapper}>
-          <div className={styles.recipeInfo}>
-    <p>
-      <strong>Cuisine Type:</strong>{" "}
-      {recipe.cuisineType
-        ?.map((type) => capitalizeFirstLetter(type))
-        .join(", ")}
-    </p>
-    <p>
-      <strong>Meal Type:</strong>{" "}
-      {recipe.mealType
-        ?.map((type) => capitalizeFirstLetter(type))
-        .join(", ")}
-    </p>
-    <div className={styles.yieldAndTime}>
-      <p><strong>Yield:</strong> {recipe.yield} people</p>
-      {recipe.totalTime && recipe.totalTime > 0 ? (
-        <p><strong>Time:</strong> {recipe.totalTime} min</p>
-      ) : null}
-    </div>
-  </div>
-          <h2>Ingredients</h2>
-          <a
-            href={recipe.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="View full recipe in a new tab"
-          >
-            Full recipe
-          </a>
-          <ul className={styles.ingredientsList} role="list">
-            {recipe.ingredientLines.map((line, index) => (
-              <li key={index}>{line}</li>
-            ))}
-          </ul>
-          <h3>Unit Converter</h3>
-          <UnitConverter />
-
-          {recipe.totalNutrients && (
-            <div className={styles.nutrients} aria-live="polite">
-              <h3>Nutrients</h3>
-              {["CHOCDF", "PROCNT", "FAT"].map((key) => {
-                const nutrient = recipe.totalNutrients[key];
-                return nutrient ? (
-                  <p key={key}>
-                  <strong>{nutrient.label}:</strong> {Math.round(nutrient.quantity)}{" "}
-                  {nutrient.unit}
-                  </p>
-                ) : null;
-              })}
+            <div className={styles.recipeImg}>
+              <img src={recipe.image} alt={`Image of ${recipe.label}`} />
             </div>
-          )}
-          </section>
+
+            <section className={styles.recipeInfoWrapper}>
+              <div className={styles.recipeInfo}>
+                <p>
+                  <strong>Cuisine Type:</strong>{" "}
+                  {recipe.cuisineType
+                    ?.map((type) => capitalizeFirstLetter(type))
+                    .join(", ")}
+                </p>
+                <p>
+                  <strong>Meal Type:</strong>{" "}
+                  {recipe.mealType
+                    ?.map((type) => capitalizeFirstLetter(type))
+                    .join(", ")}
+                </p>
+                <div className={styles.yieldAndTime}>
+                  <p>
+                    <strong>Yield:</strong> {recipe.yield} people
+                  </p>
+                  {recipe.totalTime && recipe.totalTime > 0 ? (
+                    <p>
+                      <strong>Time:</strong> {recipe.totalTime} min
+                    </p>
+                  ) : null}
+                </div>
+              </div>
+              <h2>Ingredients</h2>
+              <a
+                href={recipe.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="View full recipe in a new tab"
+              >
+                Full recipe
+              </a>
+              <ul className={styles.ingredientsList} role="list">
+                {recipe.ingredientLines.map((line, index) => (
+                  <li key={index}>{line}</li>
+                ))}
+              </ul>
+              <h3>Unit Converter</h3>
+              <UnitConverter />
+
+              {recipe.totalNutrients && (
+                <div className={styles.nutrients} aria-live="polite">
+                  <h3>Nutrients</h3>
+                  {["CHOCDF", "PROCNT", "FAT"].map((key) => {
+                    const nutrient = recipe.totalNutrients[key];
+                    return nutrient ? (
+                      <p key={key}>
+                        <strong>{nutrient.label}:</strong>{" "}
+                        {Math.round(nutrient.quantity)} {nutrient.unit}
+                      </p>
+                    ) : null;
+                  })}
+                </div>
+              )}
+            </section>
           </div>
-          <Tags 
-            title="Tags" 
-            tags={recipe.healthLabels} 
-            tagClassName={styles.healthLabelTag} 
+          <Tags
+            title="Tags"
+            tags={recipe.healthLabels}
+            tagClassName={styles.healthLabelTag}
           />
         </div>
       </div>
-      
     </div>
-    
   );
 }
