2b6cdb8d67ac27284a35236b2647a7d7bc7aa800
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-28 18:46:42 +0200

    changed name on css module

--- src/components/RecipeSearch/RecipeSearch.jsx
+++ src/components/RecipeSearch/RecipeSearch.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchRecipes } from "../../api/fetchRecipes";
 import MealCard from "../MealCard/MealCard";
-import styles from './Recipe.module.css';
+import styles from "./RecipeSearch.module.css";
 import Button from "../Button/Button";
 
 export default function RecipeSearch() {
@@ -27,7 +27,7 @@ export default function RecipeSearch() {
     <div>
       <main>
         <div className={styles.title}>
-        <h1>recipe finder</h1>
+          <h1>recipe finder</h1>
         </div>
         <section className={styles.searchContainer}>
           <div className={styles.inputContainer}>
@@ -44,22 +44,19 @@ export default function RecipeSearch() {
             <Button title="search" onClick={handleSearch} size="large" />
           </div>
         </section>
-        <div className={styles.loading}>
-        {loading && <p>Loading...</p>}
-        </div>
-     
-      
-      <section className={styles.mealCardGrid}>
-        {recipes.map((recipe) => (
-          <div 
-            key={recipe.uri} 
-            onClick={() => handleRecipeClick(recipe)} 
-            style={{ cursor: "pointer" }}
-          >
-            <MealCard recipe={recipe} />
-          </div>
-        ))}
-      </section>
+        <div className={styles.loading}>{loading && <p>Loading...</p>}</div>
+
+        <section className={styles.mealCardGrid}>
+          {recipes.map((recipe) => (
+            <div
+              key={recipe.uri}
+              onClick={() => handleRecipeClick(recipe)}
+              style={{ cursor: "pointer" }}
+            >
+              <MealCard recipe={recipe} />
+            </div>
+          ))}
+        </section>
       </main>
     </div>
   );
